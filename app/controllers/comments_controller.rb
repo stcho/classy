76dc75b68eb66e14a17b79a1f870d4424353c325
Thomas Cho <tomcho123@gmail.com>
2013-12-08 14:41:39 -0700

    Initial Controller methods for Feeds and Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,22 @@
 class CommentsController < ApplicationController
+  
+  before_action :authenticate_user!
+  
   def new
   end
 
   def create
+    @feed = Feed.find(params[:feed_id])
+    @comment = @feed.comments.create(params[:comment].permit(:commenter, :body))
+    @comment.user = current_user
+    @comment.save
+    
+    respond_to do |format|
+      format.js 
+      format.html { 
+        redirect_to feed_path(@feed) 
+      }
+    end
   end
 
   def update
@@ -12,6 +26,16 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @feed = Feed.find(params[:feed_id])
+    @comment = @feed.comments.find(params[:id])
+    @comment.destroy
+    
+    respond_to do |format|
+      format.js
+      format.html { 
+        redirect_to feed_path(@feed)
+      }
+    end
   end
 
   def index
@@ -19,4 +43,5 @@ class CommentsController < ApplicationController
 
   def show
   end
+  
 end
--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,22 +1,53 @@
 class FeedsController < ApplicationController
+  
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
+  
   def new
+    @feed = Feed.new
   end
 
   def create
+    @feed = Feed.new( feed_params )
+    @feed.user = current_user
+    if @feed.save
+      redirect_to @feed
+    else
+      render 'new'
+    end
   end
 
   def update
+    @feed = Feed.find( params[:id] )
+    if @feed.update_attributes( feed_params )
+      redirect_to @feed
+    else
+      render 'edit'
+    end
   end
 
   def edit
+    @feed = Feed.find( params[:id] )
   end
 
   def destroy
+    @feed = Feed.find( params[:id] )
+    @feed.destroy
+    redirect_to feeds_path
   end
 
   def index
+    @feeds = Feed.order( 'created_at DESC' ).limit( @feeds_per_page )
+
   end
 
   def show
+    @feed = Post.find( params[:id] )
+
   end
+  
+  private
+    def feed_params
+      params.require(:feed).permit( :text )
+    end
+    
 end
