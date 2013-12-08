22ca7932b72fdf763f83e4a25fb461c785b18fa4
Thomas Cho <tomcho123@gmail.com>
2013-12-08 16:51:47 -0700

    Added views for Feeds and Topics

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Classy::Application.routes.draw do
   devise_for :users
   resources :users
   
-  resources :feeds do
-    resources :comments
+  resources :topics do
+    resources :feeds do
+      resources :comments
+    end
   end
   
-  root 'feeds#index'
+  root 'topics#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
