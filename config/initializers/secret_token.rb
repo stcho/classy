125aec81388f50aadfb75ae51181460b71f3fa00
Thomas Cho <tomcho123@gmail.com>
2013-12-08 13:23:28 -0700

    Initial commit for Classy App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Classy::Application.config.secret_key_base = '5482c733b357a0927d3e8f931fbc3c09eaf7c8e4042e007cbc49855847c3f79380d85b5f0c8c84f37a9a871f7df82da3d4f502f79c701ea67f563c2f8e2cdb53'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Classy::Application.config.session_store :cookie_store, key: '_classy_session'
