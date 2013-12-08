31021277638819712438b646c9120e7ec2d318cb
Thomas Cho <tomcho123@gmail.com>
2013-12-08 14:15:38 -0700

    Ran migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Classy::Application.routes.draw do
   
   devise_for :users
-  # resources :feeds do
-  #   resources :comments
-  # end
-  # 
-  # devise_for :users
-  # resources :users
-  # 
-  # root 'feeds#index'
+  resources :users
+  
+  resources :feeds do
+    resources :comments
+  end
+  
+  root 'feeds#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
